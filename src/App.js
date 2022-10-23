2805a13f4c784d1bc6fdbe6cb90a80e3945ceb5f
Saaim-K <saaimkhan221@gmail.com>
2022-10-23 12:04:35 +0500

    NEWS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 export default function App() {
+  const [topic, setTopic] = useState("");
+
+
+
+  const getNews = (e) => {
+    e.preventDefault();
+    const options = {
+      method: 'GET',
+      url: 'https://bing-news-search1.p.rapidapi.com/news/search',
+      params: { q: topic, freshness: 'Day', textFormat: 'Raw', safeSearch: 'Off' },
+      headers: {
+        'X-BingApis-SDK': 'true',
+        'X-RapidAPI-Key': '8f6669f19bmshba740bef890ea3bp1e4445jsn0aeb066bb324',
+        'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+      }
+    };
+
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }
+
+
   return (
     <>
-    
+
+
+
+      <form onSubmit={getNews}>
+        <input type="text" onChange={(e) => {
+          setTopic(e.target.value)
+
+        }} />
+        <button type='submit'>Get News</button>
+      </form>
+
+
+
+
     </>
   );
 }
