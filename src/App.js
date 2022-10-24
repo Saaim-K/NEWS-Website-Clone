4ed3d760583243c7ac702a9aa40acdb89e766124
Saaim-K <saaimkhan221@gmail.com>
2022-10-24 20:42:29 +0500

    NEWS

--- src/App.js
+++ src/App.js
@@ -79,12 +79,33 @@ export default function App() {
       {(loading) ? <div className='loader'><div className='pac-man' /></div> : ""}
 
       <div>{data.map(news => (
+
+
+
+
+
+
+
+
+
+
+
+
         <div key={news?.name}>
-          <h1>{news?.name}</h1>
-          <span>{moment(news?.datePublished).format('Do MMMM YYYY, h:mm a')}</span>
-          <span>{news?.description}</span>
-          <img src={news?.image?.thumbnail?.contentUrl.replace("&pid=News", "").replace("pid=News&", "").replace("pid=News", "")} alt="" />
-          <span>{news?.url}</span>
+          <div className="projcard-container">
+
+            <div className="projcard projcard-blue">
+              <div className="projcard-innerbox">
+                <img className="projcard-img" src={news?.image?.thumbnail?.contentUrl.replace("&pid=News", "").replace("pid=News&", "").replace("pid=News", "")} alt='saaim' />
+                <div className="projcard-textbox">
+                  <div className="projcard-title">{news?.name}</div>
+                  <div className="projcard-subtitle">{moment(news?.datePublished).format('Do MMMM YYYY, h:mm a')}</div>
+                  <div className="projcard-bar"></div>
+                  <div className="projcard-description">{news?.description}</div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       )
       )
@@ -93,26 +114,30 @@ export default function App() {
 
 
 
+
+
+
+
     </>
   );
 }
 
-//First of all there is a form consisting of one input tag and one button.
-//The input tag has an onchange event that triggers as soon as something is typed in it
-//The onchange function gets the value from the input tag using e.target.value and set it in setTopic
-//The set topic has an initial value of topic that is empty string
-//After the onchange event is triggered the setTopic hook changes the topic value to the query typed in input
-//Both topic and settopic are in useState hook
-//As the button in the input tag clicked the form runs a getNews function.
-//The getNews has a parameter e and e.preventDefault() to stop the reloading of page onSubmit of form
-//Then data is fetched using API
-//The data that we got is of the topic that we typed in the search
-//In the response of the api call we setData to response.data.value in data
-//Now are data from api is saved in an array named data
-//
-//
-//
-//
-//
-//
-//
+  //First of all there is a form consisting of one input tag and one button.
+  //The input tag has an onchange event that triggers as soon as something is typed in it
+  //The onchange function gets the value from the input tag using e.target.value and set it in setTopic
+  //The set topic has an initial value of topic that is empty string
+  //After the onchange event is triggered the setTopic hook changes the topic value to the query typed in input
+  //Both topic and settopic are in useState hook
+  //As the button in the input tag clicked the form runs a getNews function.
+  //The getNews has a parameter e and e.preventDefault() to stop the reloading of page onSubmit of form
+  //Then data is fetched using API
+  //The data that we got is of the topic that we typed in the search
+  //In the response of the api call we setData to response.data.value in data
+  //Now are data from api is saved in an array named data
+  //
+  //
+  //
+  //
+  //
+  //
+  //
